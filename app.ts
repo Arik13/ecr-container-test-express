33cebbfdbad2aa16f58a28320fb261f19dc5f847
Arik Dicks <ariksven@gmail.com>
2021-09-18 12:26:48 -0700

    testing sockets

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import "module-alias/register";
 import * as express from 'express';
 import * as mongoose from "mongoose";
-
 const app = express();
+import * as httpPkg from "http";
+import * as socketio from "socket.io";
+let server = new httpPkg.Server(app);
+
 const port = 3000;
 
 interface TestDoc extends mongoose.Document {
@@ -14,16 +17,25 @@ interface TestDoc extends mongoose.Document {
 
 const TestSchema = new mongoose.Schema<TestDoc>({
     test: String,
-}, { collection: "Test", timestamps: {createdAt: "createdAt"}});
+}, { collection: "Test", timestamps: { createdAt: "createdAt" } });
 const TestModel = mongoose.model<TestDoc>("Test", TestSchema);
-
+let io = new socketio.Server(server);
 // const TestModel = mongoose.model('test', yourSchema);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
 
-app.get('/', async (req, res) => {
-    let query = await TestModel.find();
-    let response = `<h1>Hello World!</h1> ${query}`;
-    res.send(response);
-    // res.send("<h1>Hello World! (Test)</h1>");
+// app.get('/', async (req, res) => {
+//     let query = await TestModel.find();
+//     let response = `<h1>Hello World!</h1> ${query}`;
+//     res.send(response);
+//     // res.send("<h1>Hello World! (Test)</h1>");
+// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 // app.listen(port, () => {
@@ -41,7 +53,7 @@ const bootServer = async () => {
         authSource: "admin",
         readPreference: "primary",
     });
-    app.listen(port);
+    server.listen(port);
     console.info("Connected to mongodb");
     console.info(`Server active on port ${port}`);
 }
